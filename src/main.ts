95fa80954e4a6dca3dc2a1382428ea3f8f2f21f6
Chad Smith <smith.chad12@gmail.com>
2023-05-16 23:18:54 -0500

    add commander and basic program details

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,18 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { Command } from "commander";
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const program = new Command()
+                  .name("DB Archiver")
+                  .description("small utility program designed to create archives of databases");
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+program.command('archive')
+       .description("Creates an archive/backup of the database given")
+       .argument('<string>', 'name of backup')
+       .option('--host', 'host of the database')
+       .option('--user', 'username to use to connect')
+       .option('--password', 'password of the user to connect')
+       .action((str, options) => {
+        console.log(options)
+        console.log(str)
+       })
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+program.parse();
